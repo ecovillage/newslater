9c813890e947c2ce4c4d64233849267d3eb1e9c4
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-01-09 19:25:45 +0100

    add presence-validation to Section#heading, add kind enum

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,4 +2,12 @@ class Section < ApplicationRecord
   has_one_attached :image
 
   belongs_to :newsletter, required: true
+
+  validate :presence, :heading
+
+  enum kind: {
+    full:   "full",
+    double: "double",
+    short:  "short"
+  }
 end
